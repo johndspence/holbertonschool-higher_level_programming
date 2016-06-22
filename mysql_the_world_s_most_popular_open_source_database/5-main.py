ca851053f34e9dafdb869f612008fb360d5ff8fb
John Spence <johndspence@gmail.com>
2016-06-21 23:15:23 -0700

    Completed mandatory

--- mysql_the_world_s_most_popular_open_source_database/5-main.py
+++ mysql_the_world_s_most_popular_open_source_database/5-main.py
@@ -1,20 +1,50 @@
 #!/usr/bin/python
 
+#mysql python connection object
 import mysql.connector
 
+#connect to database
 connection = mysql.connector.connect(user='student', password='aLQQLXGQp2rJ4Wy5', host='173.246.108.142',port='3306',
 database='Project_169')
 
+#use cursor method
 cursor = connection.cursor()
 
-query = ("SELECT TVShow.name FROM TVShow ORDER BY TVShow.name ASC")
-cursor.execute(query)
+#1st query for show
+get_show_query = ("SELECT TVShow.name FROM TVShow ORDER BY TVShow.name ASC")
+cursor.execute(get_show_query)
 
+#Put shows into an object
 TVShow_names = cursor.fetchall()
 
-for (name, season, episode_number, episode_name) in cursor:
-	print str(name) + ":"
-	print season
+#Loop throught TVShow_names object, printing each one
+for TVShowname in TVShow_names:
+	print TVShowname[0] + ":"
+
+	#2nd query for seasons, also put into an object
+	get_season_query = ("SELECT Season.number FROM Season \
+	JOIN TVShow on Season.tvshow_id = TVShow.id \
+	WHERE TVShow.name = '" + TVShowname[0] + "' ORDER BY Season.number ASC;")
+	cursor.execute(get_season_query)
+	Seasons = cursor.fetchall()
+
+	#Loop throught Seasons object and print each Season
+	for Season in Seasons:
+		print "\tSeason " + str(Season[0]) + ":"
+
+		#Third query for episodes, also put into object
+		get_episode_query = ("SELECT Episode.number, Episode.name \
+		FROM Episode JOIN Season on Season.id = Episode.season_id \
+		JOIN TVShow on Season.tvshow_id = TVShow.id \
+		WHERE Season.number = '" + str(Season[0]) + "'\
+		AND TVShow.name = '" + TVShowname[0] + "'\
+		ORDER BY Episode.number;")
+
+		cursor.execute(get_episode_query)
+		Episodes = cursor.fetchall()
+
+		#Tab twice and print each episode 0 1 ;2, 3 etc
+		for episode in Episodes:
+			print "\t\t" + str(episode[0]) + ": " + (episode[1])
 
 connection.close()
-print "I think everything worked"
